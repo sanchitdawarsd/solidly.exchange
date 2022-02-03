f2e42b568af8cfe882a82887a2771ed196097aa8
Anton Nell <antonnell89@gmail.com>
2022-02-03 08:07:58 +0200

    Changed copy from pool to liquidity pair
    Signed-off-by: Anton Nell <antonnell89@gmail.com>

--- pages/vote/index.js
+++ pages/vote/index.js
@@ -51,7 +51,7 @@ function Vote({ changeTheme }) {
           <div className={classes.contentFloat}>
           <Typography className={classes.mainHeadingNC} variant='h1'>Vote</Typography>
           <Typography className={classes.mainDescNC} variant='body2'>
-            Use your veSolid to vote for your selected pool’s rewards distribution or create a bribe to encourage others to do the same.
+            Use your veSolid to vote for your selected liquidity pair’s rewards distribution or create a bribe to encourage others to do the same.
           </Typography>
           <Button
             disableElevation
