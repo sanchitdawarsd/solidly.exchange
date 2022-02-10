873ce3148470bde33bd02d0500e983c2ee2a981a
Anton Nell <antonnell89@gmail.com>
2022-02-10 07:44:17 +0200

    Updated to production release contracts.
    Signed-off-by: Anton Nell <antonnell89@gmail.com>

--- stores/constants/contracts.js
+++ stores/constants/contracts.js
@@ -1,17 +1,17 @@
 import abis from "../abis";
 
-export const GOV_TOKEN_ADDRESS = '0xBB5298e572519DaE7784f533B5E76F37855b1A4A'
+export const GOV_TOKEN_ADDRESS = '0x888EF71766ca594DED1F0FA3AE64eD2941740A20'
 export const GOV_TOKEN_NAME = 'Solid'
 export const GOV_TOKEN_SYMBOL = 'SOLID'
 export const GOV_TOKEN_DECIMALS = 18
-export const GOV_TOKEN_LOGO = 'https://prod.solidly.exchange/logo.svg'
+export const GOV_TOKEN_LOGO = 'https://prod.solidly.exchange/Solidly-O.svg'
 export const GOV_TOKEN_ABI = abis.tokenABI
 
-export const VE_TOKEN_ADDRESS = '0xC34A4644Bb1deea88e4045Ce3C38A0261E41FC9f'
+export const VE_TOKEN_ADDRESS = '0xcBd8fEa77c2452255f59743f55A3Ea9d83b3c72b'
 export const VE_TOKEN_NAME = 'veNFT'
 export const VE_TOKEN_SYMBOL = 'veNFT'
 export const VE_TOKEN_DECIMALS = 18
-export const VE_TOKEN_LOGO = 'https://prod.solidly.exchange/logo.svg'
+export const VE_TOKEN_LOGO = 'https://prod.solidly.exchange/Solidly-O.svg'
 export const VE_TOKEN_ABI = abis.veTokenABI
 
 export const WFTM_ADDRESS = '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83'
@@ -26,17 +26,17 @@ export const FTM_SYMBOL = 'FTM'
 export const FTM_DECIMALS = 18
 export const FTM_LOGO = 'https://assets.coingecko.com/coins/images/4001/large/Fantom.png'
 
-export const FACTORY_ADDRESS = '0x117F6F61e797E411Ea92F0ea1555c397Ecf17939'
+export const FACTORY_ADDRESS = '0x3fAaB499b519fdC5819e3D7ed0C26111904cbc28'
 export const FACTORY_ABI = abis.factoryABI
 
-export const ROUTER_ADDRESS = '0xCAE00F31F7cB5A78450Ca119fc2D0e7bbaEF0439'
+export const ROUTER_ADDRESS = '0xa38cd27185a464914D3046f0AB9d43356B34829D'
 export const ROUTER_ABI = abis.routerABI
 
-export const VE_DIST_ADDRESS = '0xA4229Db9b0bAB36b212ABe09C4Be1b76E5629906'
+export const VE_DIST_ADDRESS = '0xA5CEfAC8966452a78d6692837b2ba83d19b57d07'
 export const VE_DIST_ABI = abis.veDistABI
 
-export const VOTER_ADDRESS = '0xfF61f4aC23a3d8b5D1f21C231282A96deF3855D6'
-export const VOTER_ABI = abis.gaugesABI
+export const VOTER_ADDRESS = '0xdC819F5d05a6859D2faCbB4A44E5aB105762dbaE'
+export const VOTER_ABI = abis.voterABI
 
 export const ERC20_ABI = abis.erc20ABI
 export const PAIR_ABI = abis.pairABI
